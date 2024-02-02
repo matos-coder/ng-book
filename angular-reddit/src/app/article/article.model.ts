f21803269699716870852b37a878c0f658d09c71
matos <matiasashenafi0@gmail.com>
2024-02-02 11:04:31 +0300

    finished the redit app

--- angular-reddit/src/app/article/article.model.ts
+++ angular-reddit/src/app/article/article.model.ts
@@ -1,21 +1,31 @@
+import { error } from "console";
 
 
 export class Article {
-   title: string;
-   link: string;
-   votes: number;
+  title: string;
+  link: string;
+  votes: number;
 
   constructor(title: string, link: string, votes?: number) {
-   this.title = title;
-   this.link = link;
-   this.votes = votes || 0;
-   }
-   voteUp(): void {
-     this.votes += 1;
-     }
+    this.title = title;
+    this.link = link;
+    this.votes = votes || 0;
+  }
+  voteUp(): void {
+    this.votes += 1;
+  }
 
-    voteDown(): void {
-     this.votes -= 1;
-     }
+  voteDown(): void {
+    this.votes -= 1;
+  }
+  domain(): string | null {
+    try {
+      const domainAndPath: string = this.link.split('//')[1];
+      const protocol: string = this.link.split('//')[0];
+      return protocol + '//' + domainAndPath.split('/')[0];
+    } catch (err) {
+      return null;
+    }
+  }
 
-   }
+}
